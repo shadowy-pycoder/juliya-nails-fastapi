cdfee5dfcce6e8a22d712b1c62f8ba0565ea75dd
shadowy-pycoder <shadowy-pycoder@example.com>
2024-01-30 08:07:16 +0300

    refactored 3

--- src/core/conf/__init__.py
+++
@@ -1,6 +0,0 @@
-# ruff: noqa: F401
-from .auth_config import AuthConfig
-from .db_config import DBConfig
-from .email_config import EmailConfig
-from .redis_config import RedisConfig
-from .server_config import ServerConfig
